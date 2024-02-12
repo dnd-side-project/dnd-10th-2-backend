e4b31b8e537b55d7aa5132307d520ab29969351f
FacerAin <ywsong.dev@kakao.com>
2024-02-12 19:51:25 +0900

    [#28] feat: Add getMeetingMembers

--- src/main/java/org/dnd/timeet/meeting/domain/MeetingRepository.java
+++ src/main/java/org/dnd/timeet/meeting/domain/MeetingRepository.java
@@ -1,8 +1,13 @@
 package org.dnd.timeet.meeting.domain;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MeetingRepository extends JpaRepository<Meeting, Long> {
 
-//    Optional<Timer> findByUserId(Long id);
+    @Query("select m from Meeting m join fetch m.participants p join fetch p.member where m.id = :meetingId")
+    Optional<Meeting> findByIdWithParticipantsAndMembers(@Param("meetingId") Long meetingId);
+
 }
