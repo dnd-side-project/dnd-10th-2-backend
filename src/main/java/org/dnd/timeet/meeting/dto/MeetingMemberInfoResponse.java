fd62d71a6c7e7c94d6927ab60daf6ef79ee8edc8
Mint <audwl03071@gmail.com>
2024-02-21 17:11:39 +0900

    [#-] feat: Generate random imageNum for members

--- src/main/java/org/dnd/timeet/meeting/dto/MeetingMemberInfoResponse.java
+++ src/main/java/org/dnd/timeet/meeting/dto/MeetingMemberInfoResponse.java
@@ -2,6 +2,7 @@ package org.dnd.timeet.meeting.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
+import java.util.Random;
 import lombok.Getter;
 import lombok.Setter;
 import org.dnd.timeet.member.domain.Member;
@@ -35,13 +36,18 @@ public class MeetingMemberInfoResponse {
 
         @Schema(description = "사용자 id", nullable = false, example = "12")
         private Long id;
+
         @Schema(description = "사용자 이름", nullable = false, example = "green12")
         private String nickname;
+
+        @Schema(description = "이미지 번호(1~12 사이 랜덤)", nullable = false, example = "2")
+        private Long imageNum;
 
 
         public MeetingMemberDetailResponse(Member member) {
             this.id = member == null ? null : member.getId();
             this.nickname = member == null ? null : member.getName();
+            this.imageNum = new Random().nextLong(12) + 1;
         }
     }
 }
